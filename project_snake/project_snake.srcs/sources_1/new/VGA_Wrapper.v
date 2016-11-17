9590b2fe3019b44967e5fd1f667fb4a8b0633489
ACER-BLUES\Blues Jiang <victor1994y@qq.com>
2016-11-18 07:07:19 +0800

    no message

---
+++ project_snake/project_snake.srcs/sources_1/new/VGA_Wrapper.v
@@ -0,0 +1,142 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.11.2016 10:56:22
+// Design Name: 
+// Module Name: VGA_Wrapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module VGA_Wrapper(
+
+                    input            CLK,
+                    input     [11:0] COLOUR_IN,
+                    output    [11:0] COLOUR_OUT,
+                    output           HS,
+                    output           VS
+                    );
+                    
+    //Outputs  address preceded by wire
+    wire  [9:0]  ADDRE;
+    wire  [9:0]  ADDRV;
+    
+    //Define the value to get colour
+    reg  [11:0]  change_colour_count_out;
+    reg  [11:0] Colour; 
+    reg [11:0] X;
+    
+    //to let the cube move along the border
+    reg  [10:0]  H;
+    reg   [9:0]  V;
+
+/******************************************************************************************************************************************************************/
+/******************************************************************************************************************************************************************/
+ 
+   //Instantiatiation of the vga_control 
+   vga_control(
+              .CLK(CLK),
+              .COLOUR_IN(Colour),
+              .COLOUR_OUT(COLOUR_OUT),
+              .HS(HS),
+              .VS(VS),
+              .ADDRE(ADDRE),
+              .ADDRV(ADDRV)
+              );
+   
+  //Instantiation for a 100HZ clock                   
+    Generic_counter # (.COUNTER_WIDTH(21),
+                        .COUNTER_MAX(999999)
+                       )
+                       Slower_1(
+                              .CLK(CLK),
+                              .RESET(1'b0),
+                              .ENABLE_IN(1'b1),
+                              .TRIG_OUT(Tigg_out_100)  
+                              );
+                              
+    //Instantiation for a 1HZ clock                           
+    Generic_counter # (.COUNTER_WIDTH(8),
+                       .COUNTER_MAX(99)
+                        )
+                        Slower_2(
+                             .CLK(Tigg_out_100),
+                             .RESET(1'b0),
+                             .ENABLE_IN(1'b1),
+                             .TRIG_OUT(Tigger_out_1) 
+                             );
+                               
+ /******************************************************************************************************************************************************************/
+ /******************************************************************************************************************************************************************/
+                                                                              
+   //a changing colour which will change not in order every second                           
+  always@(posedge  Tigger_out_1)begin
+               if(  change_colour_count_out >= 12'b111111111111)
+                   change_colour_count_out <= 0;
+               else  change_colour_count_out <= change_colour_count_out + 12'b000000000011;
+               end  
+                
+  //Because of  Frequency is too big for the board, so it become a different colour             
+  always@(posedge  CLK)begin
+               if(  X >= 12'b111111111111)
+                    X <= 0;
+               else  X <= X + 12'b000000000001;
+               end  
+               
+   //Define a counter to shift in Horizontal direction                           
+   always@(posedge  Tigger_out_1)begin
+                if(  H >= 640)
+                     H <= 0;
+                else  H <= H + 1;
+                end    
+   
+   //Define a counter to shift in Vertical direction              
+   always@(posedge  Tigger_out_1)begin
+                if( V >= 480)
+                    V <= 0;
+                else  V<= V + 1;
+                end                
+       
+  /******************************************************************************************************************************************************************/
+  /******************************************************************************************************************************************************************/            
+                                                            
+   always@(posedge CLK)begin
+    //define the cube area and add the variable to move and change the colour every second
+    if (((ADDRE >= H)&&(ADDRE <= 10+H)&&(ADDRV >= 0)&&(ADDRV <= 10))
+    ||((ADDRE >= 0)&&(ADDRE <= 10)&&(ADDRV <= 480-V)&&(ADDRV >= 470-V))
+    ||((ADDRE >= 630)&&(ADDRE <= 640)&&(ADDRV >= 0+V)&&(ADDRV <= 10+V))
+    ||((ADDRE >= 630-H)&&(ADDRE <= 640-H)&&(ADDRV <= 480)&&(ADDRV >= 470)))
+    Colour <= change_colour_count_out;
+    
+    //define the first name area and change colour
+    else if (((ADDRE >= 220)&&(ADDRE <= 420)&&(ADDRV >= 130)&&(ADDRV <= 150))
+    ||((ADDRE + ADDRV >= 550)&&(ADDRE + ADDRV<=580)&&(ADDRE>=220)&&(ADDRE<=420)&&(ADDRV>=80)&&(ADDRV<=400))
+    ||((ADDRE >= 220)&&(ADDRE <= 420)&&(ADDRV >= 340)&&(ADDRV <= 360)))
+    Colour <= change_colour_count_out + 12'b101010101010;
+    
+    //define the different circle for beautiful
+    else if ((ADDRE*ADDRE + ADDRV*ADDRV <= (15+H+V)*(15+H+V))||((ADDRE-640)*(ADDRE-640) + (ADDRV-480)*(ADDRV-480) <= (15-H-V)*(15-H-V)))
+    Colour <= change_colour_count_out + 12'b010101010101;
+    else if (((ADDRE-320)*(ADDRE-320) + (ADDRV-240)*(ADDRV-240) >=625)&&((ADDRE-320)*(ADDRE-320) + (ADDRV-240)*(ADDRV-240) <=900))
+    Colour <= X + 12'b010010100101;
+    else if ((ADDRE*ADDRE + ADDRV*ADDRV <= (15+H+V)*(15+H+V))&&((ADDRE-640)*(ADDRE-640) + (ADDRV-480)*(ADDRV-480) <= (15-H-V)*(15-H-V)))
+    Colour <= change_colour_count_out + 12'b100100100100;
+    
+    //define the background colour
+    else
+    Colour <= 12'b111111111111;
+    end
+  
+endmodule
---
+++ project_snake/project_snake.srcs/sources_1/new/snake_control.v
@@ -0,0 +1,147 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2016/11/16 15:25:29
+// Design Name: 
+// Module Name: snake_control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module snake_control(
+  input CLK,
+  input RESET,
+  //input Counter,
+  input [9:0] Pixel_Address_X,
+  input [8:0] Pixel_Address_Y,
+  input [7:0] Random_Target_Address_X,
+  input [6:0] Random_Target_Address_Y,
+  input [1:0] Direction_state,
+ // input [1:0] MSM_State,
+  output reg [11:0] Colour,
+  output reg REACHED_TARGET
+
+  );
+  
+  parameter SnakeLength = 7;
+  parameter MaxY = 120;
+  parameter MaxX = 160;
+  parameter CounterMax = 500;
+  reg Counter;
+  
+  wire [7:0] AddrX;
+  wire [6:0] AddrY;
+  
+  reg [7:0] SnakeState_X [0: SnakeLength-1];
+  reg [6:0] SnakeState_Y [0: SnakeLength-1];
+  
+  assign AddrX = Pixel_Address_X [9:2];
+  assign AddrY = Pixel_Address_Y [8:2];
+  
+  //parameter 
+  always @ ( posedge CLK ) begin
+      if (Counter>0)begin
+        Counter <= Counter - 1;
+        end
+      else begin
+        Counter <= CounterMax;
+     end
+  end
+  //Changing the position of the snake registers
+  //Shift the SnakeState X and Y
+  
+  genvar PixNo;
+  generate
+      for (PixNo = 0; PixNo < SnakeLength-1; PixNo = PixNo+1)
+      begin: PixShift
+          always@(posedge CLK) begin
+              if (RESET) begin
+                  SnakeState_X[PixNo+1] <= 80;
+                  SnakeState_Y[PixNo+1] <= 100;
+              end
+              else if (Counter == 0) begin
+                  SnakeState_X[PixNo+1] <= SnakeState_X[PixNo];
+                  SnakeState_Y[PixNo+1] <= SnakeState_Y[PixNo];
+              end
+          end
+      end
+  endgenerate
+  
+  //Replace top snake state with new one based on direction
+  
+  always@(posedge CLK)begin
+      if (RESET) begin
+          //Set the initial state of the snake
+          SnakeState_X[PixNo+1] <= 80;
+          SnakeState_Y[PixNo+1] <= 100;
+      end
+      else if (Counter == 0)begin
+          case (Direction_state)
+              2'b00   :begin//up
+                  if(SnakeState_Y[0]==0)
+                      SnakeState_Y[0] <= MaxY;
+                  else
+                      SnakeState_Y[0] <= SnakeState_Y[0] - 1;
+              end
+                  
+              2'b01   :begin//left
+                  if(SnakeState_X[0]==0)
+                      SnakeState_X[0] <= MaxX;
+                  else
+                      SnakeState_X[0] <= SnakeState_X[0] - 1;
+              end
+              
+              2'b10   :begin//right
+                  if(SnakeState_X[0]==MaxX)
+                      SnakeState_X[0] <= 0; 
+                  else
+                      SnakeState_X[0] <= SnakeState_X[0] + 1;
+              end
+              
+              2'b11   :begin//down
+                  if(SnakeState_Y[0]==MaxY)
+                      SnakeState_Y[0] <= 0;
+                  else
+                      SnakeState_Y[0] <= SnakeState_Y[0] + 1;
+              end
+                 
+          endcase
+          
+      end
+  end
+
+always@( posedge CLK ) begin
+    if ((SnakeState_X[0]==Random_Target_Address_X)&&(SnakeState_Y[0]==Random_Target_Address_Y))
+         REACHED_TARGET <= 1;
+    else
+         REACHED_TARGET <= 0;
+end 
+
+always@(posedge CLK) begin
+    if ((AddrX[0]==SnakeState_X[0]) &&(AddrY[0]==SnakeState_Y[0])
+        ||(AddrX[1]==SnakeState_X[1]) &&(AddrY[1]==SnakeState_Y[1])
+        ||(AddrX[2]==SnakeState_X[2]) &&(AddrY[2]==SnakeState_Y[2])
+        ||(AddrX[3]==SnakeState_X[3]) &&(AddrY[3]==SnakeState_Y[3])
+        ||(AddrX[4]==SnakeState_X[4]) &&(AddrY[4]==SnakeState_Y[4])
+        ||(AddrX[5]==SnakeState_X[5]) &&(AddrY[5]==SnakeState_Y[5])
+        ||(AddrX[6]==SnakeState_X[6]) &&(AddrY[6]==SnakeState_Y[6]))  
+        Colour <= 12'b000011110000;
+    else if ((AddrX==Random_Target_Address_X)&&(AddrY==Random_Target_Address_Y))
+        Colour <= 12'b111111110000;
+    else
+        Colour <= 12'b111111111111;
+    end
+             
+endmodule
